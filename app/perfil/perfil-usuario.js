894e8fc7eda855da55a3cfbae17a933daee7ce00
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-04-09 12:59:01 -0300

    feat: start of the profile model

--- app/perfil/perfil-usuario.js
+++ app/perfil/perfil-usuario.js
@@ -1,45 +1,33 @@
-import { StyleSheet, Pressable, View, Text } from "react-native";
+import { StyleSheet, Pressable, View, Text, Button, Image } from "react-native";
 import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import Footer from "../footer/footer";
 
 export default function Perfil() {
-  
-
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#151718" style="light" />
-      <Link href="/" asChild>
-        <Pressable style={styles.botao_link}>
-          <Text style={styles.botao_text}>Index</Text>
-        </Pressable>
-      </Link>
-
-      <Link href="home/tela-inicial" asChild>
-        <Pressable style={styles.botao_link}>
-          <Text style={styles.botao_text}>tela home</Text>
-        </Pressable>
-      </Link>
-
-      {/* <Link href="/" asChild>
-        <Pressable style={styles.botao_link}>
-          <Text style={styles.botao_text}>Cadastro Passageiro</Text>
-        </Pressable>
-      </Link> */}
-
+      <View>
+        <Text style={styles.title}>Seu Perfil</Text>
+        <View>
+          <Image
+            style= {styles.profile_picture}
+            source={require('./imgs/profilePicture.png')}
+          />
+        </View>
+      </View>
       <Footer/>
     </View>
   )
-
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#151718",
+    backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center"
   },
-  botao_link: {
+  botao_report: {
     backgroundColor: "gray",
     width: "70%",
     margin: 10,
@@ -47,12 +35,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
-  botao_text: {
-    color: "#FFF",
+  title: {
+    color: 'white',
+    paddingLeft: 20,
+    paddingRight: 20,
     fontSize: 25,
-    padding: 10
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 40,
+    borderColor: '#75D67F',
+    backgroundColor: '#75D67F',
+  },
+  profile_picture: {
+    borderWidth: 1,
+    borderRadius: 80,
+    width: 80,
+    height: 80,
   }
-
-
-
 })
